dca8d1d3370a49d100db1becb969c4b8861109c4
Kathan <kathanythakkar07@gmail.com>
2025-03-26 22:47:44 +0530

    corrected type error for redis client and dockerized few of the services

---
+++ Dockerfiles/monitoring-service.Dockerfile
@@ -0,0 +1,23 @@
+FROM node:20-alpine AS base
+
+FROM base AS builder
+RUN apk update && apk add --no-cache libc6-compat
+WORKDIR /app
+RUN npm install -g turbo
+COPY . .
+RUN turbo prune monitoring-service --docker
+
+FROM base AS installer
+RUN apk update && apk add --no-cache libc6-compat
+WORKDIR /app
+COPY --from=builder /app/out . 
+RUN npm ci --prefix json & npm i --prefix full && wait
+RUN npx prisma generate --schema=full/packages/prisma-client/prisma/schema.prisma
+RUN npm run build --prefix full
+
+FROM base AS runner
+WORKDIR /app
+RUN addgroup --system --gid 1001 nodejs && adduser --system --uid 1001 nodejs
+USER nodejs
+COPY --from=installer /app .
+CMD ["npm", "run", "start", "--prefix", "full/apps/monitoring-service"]
--- packages/redis-client/src/index.ts
+++ packages/redis-client/src/index.ts
@@ -4,20 +4,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Constants
+const RESPONSE_TIME_THRESHOLD: number = 750;
+const WEBSITE_CACHE_KEY: string = 'active-websites';
+const WEBSITE_CACHE_TTL: number = 1800; 
+const ALERT_COOLDOWN_KEY_PREFIX: string = 'alert-cooldown';
+const ALERT_COOLDOWN_PERIOD: number = 1800;
+
 // Redis client setup
-const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
-const redisClient = new IORedis(redisUrl, {
-    maxRetriesPerRequest: null, // Prevents retry issues in BullMQ
+const redisUrl: string = process.env.REDIS_URL || 'redis://localhost:6379';
+const redisClient: IORedis = new IORedis(redisUrl, {
+    maxRetriesPerRequest: null,
 });
 
-const RESPONSE_TIME_THRESHOLD = 750;
-const WEBSITE_CACHE_KEY = 'active-websites';
-const WEBSITE_CACHE_TTL = 1800; 
-const ALERT_COOLDOWN_KEY_PREFIX = 'alert-cooldown';
-const ALERT_COOLDOWN_PERIOD = 1800;
-
-
-redisClient.on('error', (err) => {
+redisClient.on('error', (err: Error) => {
     console.error('Redis Client Error', err);
 });
 
@@ -26,12 +26,16 @@ redisClient.on('connect', () => {
 });
 
 // BullMQ setup
-const monitoringQueue = new Queue('monitoring-queue', {
+const monitoringQueue: Queue = new Queue('monitoring-queue', {
     connection: redisClient,
 });
 
-
-export { redisClient, monitoringQueue, RESPONSE_TIME_THRESHOLD, WEBSITE_CACHE_KEY, WEBSITE_CACHE_TTL, ALERT_COOLDOWN_KEY_PREFIX,ALERT_COOLDOWN_PERIOD };
-
-
-
+export {
+    redisClient,
+    monitoringQueue,
+    RESPONSE_TIME_THRESHOLD,
+    WEBSITE_CACHE_KEY,
+    WEBSITE_CACHE_TTL,
+    ALERT_COOLDOWN_KEY_PREFIX,
+    ALERT_COOLDOWN_PERIOD
+};
